a9021bfdd97379659651890f5548db43271cab1a
AveryDeemer <averydeemer@gmail.com>
2023-08-13 01:30:47 -0400

    DatabaseContext usage (see Tables.js), 8/13/23

--- taptaprestaurant/src/pages/Tables.js
+++ taptaprestaurant/src/pages/Tables.js
@@ -3,11 +3,12 @@ import DatabaseContext from "../components/DatabaseContext";
 import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function Tables() {
-  const [data, setData] = useState({ phone: "none" });
+  const [data, setData] = useState(null);
 
   return (
     <div>
-      <h1>{data.phone}</h1>
+      {/*DatabaseContext usage demo*/}
+      <h1>{data != null ? data.phone : ""}</h1>
       <button onClick={() => console.log(data)}>Print Data</button>
       <DatabaseContext onDataChange={setData} />
       <LoginForm />
